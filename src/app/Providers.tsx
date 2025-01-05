6fe42fb84d73ed77de6ad88207679f0c7183e97b
Joshua Udo <udojoshua3@gmail.com>
2025-01-05 06:07:18 +0100

    ui, implment layout and redux setup

---
+++ src/app/Providers.tsx
@@ -0,0 +1,24 @@
+'use client';
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from '@/app/store';
+
+const Providers = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <html lang="en">
+      <Provider store={store}>
+        <PersistGate
+          loading="loading redux persist variables...."
+          persistor={persistor}
+        >
+          {children}
+        </PersistGate>
+      </Provider>
+    </html>
+  );
+};
+
+export default Providers;
