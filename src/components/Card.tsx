e11fd4bce142e18f7a7084040ea769a1d814a312
Joshua Udo <udojoshua3@gmail.com>
2025-01-05 06:12:53 +0100

    ci: implement continous integration

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
 interface CardProps {
-  mode: 'light' | 'dark';
+  mode: "light" | "dark";
   cardNumber: string;
   amount: string;
   cardName: string;
@@ -15,26 +15,26 @@ const Card: React.FC<CardProps> = ({
   expiryDate,
   cardName,
 }) => {
-  const isDarkMode = mode === 'dark';
+  const isDarkMode = mode === "dark";
 
   return (
     <div
       className={`flex justify-between flex-col 3xl:min-w-[350px] 2xl:min-w-[350px] 3xl:w-full 2xl:w-full md:w-full md:min-w-[300px] 3xl:h-[235px] 2xl:h-[235px] min-w-[265px] h-[170px] xl:h-[220px] lg:h-[200px] border border-gray-200 ${
-        isDarkMode ? 'bg-card-gradient' : 'bg-white'
+        isDarkMode ? "bg-card-gradient" : "bg-white"
       } items-center rounded-[25px]`}
     >
       <div className="flex items-center justify-between w-full 3xl:pt-6 2xl:pt-6 pt-5 3xl:px-6 2xl:px-6 px-5">
         <div className="flex flex-col">
           <p
             className={`font-normal 3xl:text-[12px] 2xl:text-[12px] text-[11px] font-lato ${
-              isDarkMode ? 'text-white' : 'text-black'
+              isDarkMode ? "text-white" : "text-black"
             }`}
           >
             Balance
           </p>
           <p
             className={`font-lato 3xl:text-[20px] 2xl:text-[20px] text-[1rem] 3xl:leading-6 2xl:leading-6 ${
-              isDarkMode ? 'text-white' : 'text-black'
+              isDarkMode ? "text-white" : "text-black"
             }`}
           >
             {amount}
@@ -42,7 +42,7 @@ const Card: React.FC<CardProps> = ({
           </p>
         </div>
         <img
-          src={isDarkMode ? '/Chip_Card.png' : '/Chip-Card-Light.png'}
+          src={isDarkMode ? "/Chip_Card.png" : "/Chip-Card-Light.png"}
           alt="Chip Card"
           className="3xl:w-[35px] 2xl:w-[35px] 3xl:h-[35px] 2xl:h-[35px] w-[29px] h-[29px]"
         />
@@ -52,14 +52,14 @@ const Card: React.FC<CardProps> = ({
         <div className="flex flex-col">
           <p
             className={`3xl:text-[12px] 2xl:text-[12px] text-[10px] ${
-              isDarkMode ? 'text-[#FFFFFFB2]' : 'text-gray-500'
+              isDarkMode ? "text-[#FFFFFFB2]" : "text-gray-500"
             }`}
           >
             CARD HOLDER
           </p>
           <p
             className={`3xl:text-[15px] 2xl:text-[15px] text-[13px] ${
-              isDarkMode ? 'text-white' : 'text-black'
+              isDarkMode ? "text-white" : "text-black"
             }`}
           >
             {cardName}
@@ -69,14 +69,14 @@ const Card: React.FC<CardProps> = ({
         <div className="flex flex-col">
           <p
             className={`3xl:text-[12px] 2xl:text-[12px] text-[10px] ${
-              isDarkMode ? 'text-[#FFFFFFB2]' : 'text-gray-500'
+              isDarkMode ? "text-[#FFFFFFB2]" : "text-gray-500"
             }`}
           >
             VALID THRU
           </p>
           <p
             className={`3xl:text-[15px] 2xl:text-[15px] text-[13px] ${
-              isDarkMode ? 'text-white' : 'text-black'
+              isDarkMode ? "text-white" : "text-black"
             }`}
           >
             {expiryDate}
@@ -87,19 +87,19 @@ const Card: React.FC<CardProps> = ({
 
       <div
         className={`w-full rounded-b-[25px] px-5 3xl:px-6 2xl:px-6 3xl:h-[70px] 2xl:h-[70px] h-[51px] flex justify-between items-center ${
-          isDarkMode ? 'bg-white-to-transparent' : 'bg-gray-100'
+          isDarkMode ? "bg-white-to-transparent" : "bg-gray-100"
         }`}
       >
         <h3
           className={`font-semibold 3xl:text-[22px] 2xl:text-[22px] text-[15px] ${
-            isDarkMode ? 'text-white' : 'text-black'
+            isDarkMode ? "text-white" : "text-black"
           }`}
         >
           {cardNumber}
           {/* 3778 **** **** 1234 */}
         </h3>
         <img
-          src={isDarkMode ? '/mastercard-dark.png' : '/mastercard-light.png'}
+          src={isDarkMode ? "/mastercard-dark.png" : "/mastercard-light.png"}
           alt="Mastercard Logo"
           className="3xl:w-[44px] 2xl:w-[44px] 3xl:h-[30px] 2xl:h-[30px] w-[27px] h-[18px]"
         />
